78fa6d2e12afa817556c3fe1db8b695b864795dd
PRAFULL KUMAR <64453103+prafullkumar60@users.noreply.github.com>
2020-05-01 22:48:44 +0530

    check given year is armstrong number or not

---
+++ LeapYear.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class LeapYear
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Enter year to check leap year");
+		Scanner sc=new Scanner(System.in);
+		int year=sc.nextInt();
+		if((year%4==0 && year%100!=0)||year%400==0)
+		{
+			System.out.println(year+" is leap year");
+		}
+		else
+			System.out.println(year+" is not leap year");
+	}
+}
