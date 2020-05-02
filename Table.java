807f117a7486b993d42d8cc46e914bba7ddcde29
PRAFULL KUMAR <64453103+prafullkumar60@users.noreply.github.com>
2020-05-02 16:32:08 +0530

    table of any number

---
+++ Table.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Table
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number to print table");
+		int n=sc.nextInt();
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(n+"x"+i+" ="+(n*i));
+		}
+	}
+}
