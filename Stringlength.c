015647d74b8eaa10d5490120b159c6266458580f
PRAFULL KUMAR <64453103+prafullkumar60@users.noreply.github.com>
2020-07-16 20:23:45 +0530

    find String length using double pointer

---
+++ Stringlength.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main()
+
+{
+	
+	char str[100],*ptr,**ptr1;
+
+	int length=0;
+	
+	printf("Enter string to find length\n");
+	
+	scanf("%s",str);
+	
+	printf("Given string is:%s\n",str);
+	
+	ptr=str;
+
+	ptr1=&ptr;
+	
+	while(**ptr1!='\0')
+
+	{
+
+	    ptr++;
+
+	    length++;
+
+	}
+	printf("Length of the string is:%d",length);
+
+}
